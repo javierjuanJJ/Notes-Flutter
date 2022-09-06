46fb46babec052460506f0414db45e697d0fb058
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-06 19:44:31 +0200

    9. HomePage (Initialize Firebase)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'firebase_options.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -79,7 +82,9 @@ class _HomePageState extends State<HomePage> {
         TextButton(onPressed: () async {
           final email = _email.text;
           final password = _password.text;
-
+          await Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          );
           final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
               email: email,
               password: password
