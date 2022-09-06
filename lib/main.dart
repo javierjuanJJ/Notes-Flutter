ee2a50c6e31a7adbca0de387557e69c287a2575c
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-06 19:26:19 +0200

    9. HomePage (Decorate TextFileds)

--- lib/main.dart
+++ lib/main.dart
@@ -25,12 +25,30 @@ class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return _HomePageState();
-  }
+  State<StatefulWidget> createState() => _HomePageState();
+
 }
 
 class _HomePageState extends State<HomePage> {
+
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,8 +56,18 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Register'),
       ),
       body: Column(children: [
-        TextField(),
-        TextField(),
+        TextField(
+          controller: _email,
+          decoration: const InputDecoration(
+            hintText: 'Enter your email here'
+          ),
+        ),
+        TextField(
+          controller: _password,
+          decoration: const InputDecoration(
+              hintText: 'Enter your password here'
+          ),
+        ),
         TextButton(onPressed: () async {}, child: const Text('Register'),
         )]
       ) ,
