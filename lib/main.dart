fc434b46932d2ce5b569e254787b43f949ced3aa
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-07 15:59:56 +0200

    12. Link Between Login and Register Views (Move VerifyEmailView to its own file)

--- lib/main.dart
+++ lib/main.dart
@@ -67,33 +67,4 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class VerifyEmailView extends StatefulWidget {
-  const VerifyEmailView({Key? key}) : super(key: key);
 
-  @override
-  State<VerifyEmailView> createState() => _VerifyEmailViewState();
-}
-
-class _VerifyEmailViewState extends State<VerifyEmailView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Verify Email'),
-      ),
-    body: Column(
-      children: [
-        Text('Please verify your email address'),
-        TextButton(
-          onPressed: () async {
-            final user = FirebaseAuth.instance.currentUser;
-            await user?.sendEmailVerification();
-          },
-          child: Text('Send email verification'),
-        ),
-      ],
-    ),
-    )
-    ;
-  }
-}
