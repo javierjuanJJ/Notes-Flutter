d0021531f922092f1479074f933869ad7cc0ce23
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-08 12:59:12 +0200

    18. Migrating to Auth Service (Future<AuthUser> logIn)

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,10 @@
+import 'package:notes/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+
+  Future<AuthUser?> logIn({
+    required String email,
+    required String password,
+  });
+}
