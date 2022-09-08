4c019e5b9bbcaa1b6a6c30e1eb123acc7d8909c2
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-08 14:50:44 +0200

    19. Unit Testing our AuthService (Clean up NotesView)

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,11 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/constants/routes.dart';
 import 'package:notes/enums/menuAction.dart';
 import 'package:notes/main.dart';
 import 'dart:developer' as devtools show log;
+
+import 'package:notes/services/auth/auth_service.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({Key? key}) : super(key: key);
@@ -25,10 +26,9 @@ class _NotesViewState extends State<NotesView> {
               switch (value) {
                 case MenuAction.logout:
                   final dialog = await showLogOutDialog(context);
-                  devtools.log(dialog.toString());
 
                   if (dialog) {
-                    await FirebaseAuth.instance.signOut();
+                    await AuthService.firebase().logOut();
                     Navigator.of(context)
                         .pushNamedAndRemoveUntil(loginRoute, (_) => false);
                   }
