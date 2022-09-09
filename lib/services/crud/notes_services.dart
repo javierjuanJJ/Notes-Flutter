3b46b77923d0b61b16ff0866f9d2e82f486e520e
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-09 10:59:22 +0200

    20. CRUD Local Storage (notes_services DatabaseNote)

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -11,7 +11,6 @@ class DatabaseUser {
       : id = map[idColumn] as int,
         email = map[emailColumn] as String;
 
-
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
@@ -28,3 +27,33 @@ class DatabaseUser {
 
 const idColumn = 'id';
 const emailColumn = 'email';
+
+@immutable
+class DatabaseNote {
+  final int id;
+  final int user_id;
+  final String text;
+
+  DatabaseNote({required this.id, required this.user_id, required this.text});
+
+  DatabaseNote.fromRaw(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        user_id = map[userIdColumn] as int,
+        text = map[textColumn] as String;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DatabaseNote &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          user_id == other.user_id &&
+          text == other.text;
+
+  @override
+  String toString() {
+    return 'DatabaseNote{id: $id, user_id: $user_id, text: $text}';
+  }
+}
+const userIdColumn = 'user_id';
+const textColumn = 'text';
