cd46cc3e5f58d2a64ff0b3d1f8e753e44cadc7b0
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-09 15:25:20 +0200

    23. Preparing to Create New Notes (Add new_note_view and move notes_view)

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notes/constants/routes.dart';
 import 'package:notes/services/auth/auth_service.dart';
 import 'package:notes/views/login_view.dart';
-import 'package:notes/views/notes_view.dart';
+import 'package:notes/views/notes/notes_view.dart';
 import 'package:notes/views/register_view.dart';
 import 'package:notes/views/verify_email_view.dart';
 
