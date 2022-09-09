668190b1b6004339510c0d34fd0d37857c624b96
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-09 10:55:43 +0200

    20. CRUD Local Storage (notes_services DatabaseUser)

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -1,3 +1,30 @@
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart' show getApplicationDocumentsDirectory;
-import 'package:path_provider/path_provider.dart' show join;
+import 'package:flutter/cupertino.dart';
+
+@immutable
+class DatabaseUser {
+  final int id;
+  final String email;
+
+  DatabaseUser({required this.id, required this.email});
+
+  DatabaseUser.fromRaw(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DatabaseUser &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          email == other.email;
+
+  @override
+  String toString() {
+    return 'DatabaseUser{id: $id, email: $email}';
+  }
+}
+
+const idColumn = 'id';
+const emailColumn = 'email';
