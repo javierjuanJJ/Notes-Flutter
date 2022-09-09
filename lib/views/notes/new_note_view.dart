f694597a372e7416d35435cb952c7dbaeabac676
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-09 16:15:25 +0200

    24. Creating New Notes (dispose)

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -44,6 +44,14 @@ class _NewNoteViewState extends State<NewNoteView> {
   }
 
   @override
+  void dispose() {
+    _deleteNoteIfTextIsEmpty();
+    _saveNoteIfTextIsEmpty();
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('New Note'),),
