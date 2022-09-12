af87c131ac8fd310845de0de4fa5b8c524dcf291
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-12 14:42:52 +0200

    28. Writing Notes to Cloud Firestore (cloud_note)

---
+++ lib/services/cloud/cloud_note.dart
@@ -0,0 +1,23 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:notes/services/cloud/cloud_storage_constants.dart';
+import 'package:flutter/foundation.dart';
+
+class CloudNote {
+  final String documentId;
+  final String ownerUserId;
+  final String text;
+
+  const CloudNote({
+    required String this.documentId,
+    required String this.ownerUserId,
+    required String this.text,
+  });
+
+  CloudNote.fromSnapshot(QueryDocumentSnapshot<Map<String, dynamic>> snapshot)
+      : documentId = snapshot.id,
+        ownerUserId = snapshot.data()[ownerUserIdFieldName],
+        text = snapshot.data()[textFieldName] as String;
+
+}
+
