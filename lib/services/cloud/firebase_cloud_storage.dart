af3d682313381759bd35879454eb3c18e8fae704
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-12 14:51:13 +0200

    28. Writing Notes to Cloud Firestore (createNewNote)

---
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:notes/services/cloud/cloud_note.dart';
+import 'package:notes/services/cloud/cloud_storage_constants.dart';
+import 'package:notes/services/cloud/cloud_storage_exceptions.dart';
+
+class FirebaseCloudStorage {
+  static final FirebaseCloudStorage _shared =
+      FirebaseCloudStorage._sharedInstance();
+
+  FirebaseCloudStorage._sharedInstance();
+
+  factory FirebaseCloudStorage() => _shared;
+  final notes = FirebaseFirestore.instance.collection('notes');
+
+  void createNewNote({required String ownerUserId}) async {
+    await notes.add({
+      ownerUserIdFieldName: ownerUserId,
+      textFieldName: '',
+    });
+  }
+}
