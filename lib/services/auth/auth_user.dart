ac6c4f5ef513e9f7434e5d8420f27be4985c0f54
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-12 14:28:30 +0200

    28. Writing Notes to Cloud Firestore (CloudStorageException)

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -3,11 +3,15 @@ import 'package:flutter/cupertino.dart';
 
 @immutable
 class AuthUser {
+  final String id;
   final bool isEmailVerified;
   final String? email;
 
-  const AuthUser({required this.email, required this.isEmailVerified});
+  const AuthUser(
+      {required this.email,
+      required this.isEmailVerified,
+      required String this.id});
 
-  factory AuthUser.fromFirebase(User user) =>
-      AuthUser(isEmailVerified: user.emailVerified, email: user.email);
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+      id: user.uid, isEmailVerified: user.emailVerified, email: user.email!);
 }
